0593b3463bc78d9317a4851cac5e00ef71403d26
syedshahmirsultan <syedshahmirsultan@gmail.com>
2023-07-16 04:57:30 +0500

    update BasePath

--- src/components/shared/BasePath.ts
+++ src/components/shared/BasePath.ts
@@ -1,3 +1,4 @@
 const BASE_PATH_FORAPI = process.env.NODE_ENV ==="development"?
-"http://localhost:3000":"shahmir-ecommerce-website.vercel.app"
+"http://localhost:3000" :
+"https://shahmir-ecommerce-website.vercel.app"
 export default BASE_PATH_FORAPI;
