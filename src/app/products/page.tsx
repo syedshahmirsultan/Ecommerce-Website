476049328d3c7130b99a830035c535741e0200a6
syedshahmirsultan <syedshahmirsultan@gmail.com>
2023-07-17 01:09:50 +0500

    resolve errors

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,20 +1,45 @@
+// import BASE_PATH_FORAPI from "@/components/shared/BasePath"
+// import AllProductsCompo from "@/components/views/AllProducts";
+
+// async function fetchAllProductsData() {
+// let res =await fetch(`${BASE_PATH_FORAPI}/api/product?start=0&end=10`);
+// if(!res.ok){
+// throw new Error("Failed To Fetch")
+// }
+// return res.json()
+// }
+// const Product = async() => {
+//   const productData = await fetchAllProductsData();
+//   return (
+//     <div>
+//       <AllProductsCompo productData={productData}/>
+//     </div>
+//   )
+// }
+
+// export default Product
+
 import BASE_PATH_FORAPI from "@/components/shared/BasePath"
 import AllProductsCompo from "@/components/views/AllProducts";
 
-async function fetchAllProductsData() {
-let res =await fetch(`${BASE_PATH_FORAPI}/api/product?start=0&end=10`);
-if(!res.ok){
-throw new Error("Failed To Fetch")
-}
-return res.json()
-}
-const Product = async() => {
-  const productData = await fetchAllProductsData();
-  return (
-    <div>
-      <AllProductsCompo productData={productData}/>
-    </div>
-  )
+async function fetchAllProductData() {
+    let res = await fetch(`${BASE_PATH_FORAPI}/api/products?start=0&end=10`, {
+        next: {
+            revalidate: 120
+        }
+    })
+    if (!res.ok) {
+        throw new Error("Failed to fetch")
+    }
+    return res.json();
+};
+
+
+const Products = async () => {
+    const ProductData = await fetchAllProductData()
+    return (
+        <AllProductsCompo productData={ProductData} />
+    )
 }
 
-export default Product
+export default Products
