13410ece730ac6b1ae623e0eeeeeee121d95f202
syedshahmirsultan <syedshahmirsultan@gmail.com>
2023-07-19 23:21:47 +0500

    update search folder

--- src/app/search/[query]/page.tsx
+++ src/app/search/[query]/page.tsx
@@ -20,7 +20,7 @@ const Search = async ({ params }: { params: { query: string } }) => {
         <div
             className="grid grid-cols-2 md:grid-cols-3 py-10 lg:grid-cols-4 gap-4"
         >
-            { dataToMap && dataToMap.map((items: oneProductType, index: number) => (
+            {dataToMap && dataToMap.map((items: oneProductType, index: number) => (
                 <Card key={index} singleProductData={items} />
             ))}
         </div>
@@ -28,3 +28,39 @@ const Search = async ({ params }: { params: { query: string } }) => {
 }
 
 export default Search
+
+
+
+
+
+
+// import { oneProductType } from "@/components/utils/ProductsDataArrayAndType";
+// import { client } from "../../../../sanity/lib/client";
+// import Card from "@/components/views/Card";
+
+// async function getAllProductsForSearch() {
+//     let response = await client.fetch(`*[_type == "products"]`);
+//     return response;
+// };
+
+// const Search = async ({ params }: { params: { query: string } }) => {
+//     let slug = (params.query).toLowerCase();
+//     let data = await getAllProductsForSearch()
+//     let dataToMap = await data.filter((item: oneProductType) => {
+//         if ((item.productName).toLowerCase().indexOf(slug) >= 0) {
+//             return true
+//         }
+//         return false
+//     });
+//     return (
+//         <div
+//             className="grid grid-cols-2 md:grid-cols-3 py-10 lg:grid-cols-4 gap-4"
+//         >
+//             { dataToMap && dataToMap.map((items: oneProductType, index: number) => (
+//                 <Card key={index} singleProductData={items} />
+//             ))}
+//         </div>
+//     )
+// }
+
+// export default Search
