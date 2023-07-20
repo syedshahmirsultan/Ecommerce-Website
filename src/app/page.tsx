7d0295abaaee25ea4ed8c4e9a689fc5f2b3a4450
syedshahmirsultan <syedshahmirsultan@gmail.com>
2023-07-20 09:15:51 +0500

    update AllProducts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import ProductType from '@/components/views/ProductType'
 import { responseType } from '@/components/utils/ProductsDataArrayAndType';
 import Jewellery from '@/components/views/Jewellery';
 import Female from './female/[ftype]/page';
+import NewsLetter from '@/components/views/NewsLetter';
 
 async function fetchAllProductsData(){
   let res=await fetch(`https://${process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}.api.sanity.io/v2021-06-07/data/query/production?query=*[_type == "product"]`,{
@@ -28,6 +29,7 @@ return res.json();
     <ProductType/>
     <ProductCarousel ProductData={result}/>
     <Jewellery/>
+    <NewsLetter/>
     </div>
     
   )
--- src/components/views/Card.tsx
+++ src/components/views/Card.tsx
@@ -1,3 +1,4 @@
+"use client"
 import React, { FC } from 'react'
 import Image from 'next/image'
 import { oneProductType } from '../utils/ProductsDataArrayAndType'
