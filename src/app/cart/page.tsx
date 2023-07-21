474e604a7d5b51862a8cd5129161b4b96d022fd8
syedshahmirsultan <syedshahmirsultan@gmail.com>
2023-07-22 03:34:52 +0500

    resolve error

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -3,7 +3,7 @@ import ContextWrapper from "@/global/Context";
 
 
 async function fatchAllStoreProducts() {
-  let res = await fetch(`https://${NEXT_PUBLIC_SANITY_PROJECT_ID}.api.sanity.io/v2023-05-26/data/query/production?query=*[_type == 'product']`, {
+  let res = await fetch(`https://${process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}.api.sanity.io/v2023-05-26/data/query/production?query=*[_type == 'product']`, {
     cache: "no-store",
   })
   return res.json();
