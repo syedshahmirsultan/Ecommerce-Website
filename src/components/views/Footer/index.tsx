abe3394ff9530a5e3ac33a85d173b1ebe99b39b9
syedshahmirsultan <syedshahmirsultan@gmail.com>
2023-07-25 12:26:47 +0500

    minor changes in footer

--- src/components/views/Footer/index.tsx
+++ src/components/views/Footer/index.tsx
@@ -60,7 +60,7 @@ const Footer = () => {
       </div>
     </div>
   </div>
-  <div className="bg-gray-100">
+  <div className="bg-gray-100 overflow-hidden">
     <div className="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
       <p className="text-gray-500 text-sm text-center sm:text-left">© 2023 Syed Shahmir Sultan 
       </p>
