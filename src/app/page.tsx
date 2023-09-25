a3cc1519be67db365f293defd5fbbe1173772a9d
syedshahmirsultan <syedshahmirsultan@gmail.com>
2023-09-25 21:55:35 +0500

    resolving errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,27 +6,27 @@ import ProductCarousel from "@/components/views/ProductCarousel";
 import ProductsType from "@/components/views/ProductType";
 
 
-// async function fetchAllProductsData() {
-//   let res = await fetch(`https://${process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}.api.sanity.io/v2021-06-07/data/query/production?query=*[_type == "products"]`, {
-//     next: {
-//       revalidate: 60
-//     }
-//   });
+async function fetchAllProductsData() {
+  let res = await fetch(`https://${process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}.api.sanity.io/v2021-06-07/data/query/production?query=*[_type == "Products"]`, {
+    next: {
+      revalidate: 60
+    }
+  });
 
-//   if (!res.ok) {
-//     throw new Error("Failed to fetch")
-//   }
+  if (!res.ok) {
+    throw new Error("Failed to fetch")
+  }
 
-//   return res.json();
-// }
+  return res.json();
+}
 
   async function Home() {
-  //  let { result }: responseType = await fetchAllProductsData();
+    let { result }: responseType = await fetchAllProductsData();
   return (
     <div className="overflow-hidden">
       <Hero />
       <ProductsType />
-      {/* <ProductCarousel ProductData={result} /> */}
+     <ProductCarousel ProductData={result} />
       <Jewellery />
       <Newsletter />
     </div>
